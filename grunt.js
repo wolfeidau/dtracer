ee21e615621edf4ae0b7c5bd49743efaaf7faff2
Mark Wolfe <mark@wolfe.id.au>
2013-01-04 11:21:16 +1100

    Mocha configured and lint passed.

--- grunt.js
+++ grunt.js
@@ -1,39 +1,66 @@
-module.exports = function(grunt) {
+'use strict';
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    test: {
-      files: ['test/**/*.js']
-    },
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true
-      },
-      globals: {
-        exports: true
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg:'<json:package.json>',
+        test:{
+            files:['test/**/*.js']
+        },
+        lint:{
+            files:['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+        },
+        watch:{
+            files:'<config:lint.files>',
+            tasks:'default'
+        },
+        jshint:{
+            options:{
+                curly:true,
+                eqeqeq:true,
+                immed:true,
+                latedef:true,
+                newcap:true,
+                noarg:true,
+                sub:true,
+                undef:true,
+                boss:true,
+                eqnull:true,
+                node:true,
+                "predef":[
+                    "describe", // Used by mocha
+                    "it", // Used by mocha
+                    "before", // Used by mocha
+                    "beforeEach", // Used by mocha
+                    "after", // Used by mocha
+                    "afterEach"      // Used by mocha
+                ]
+            },
+            globals:{
+                exports:true
+            }
+        },
+        simplemocha:{
+            all:{
+                src:'test/**/*.js',
+                options:{
+                    globals:['should'],
+                    timeout:3000,
+                    ignoreLeaks:false,
+                    ui:'bdd',
+                    reporter:'spec'
+                }
+            }
+        }
 
-  // Default task.
-  grunt.registerTask('default', 'lint test');
+    });
+
+    grunt.loadNpmTasks('grunt-simple-mocha');
+
+    // Default task.
+    grunt.registerTask('default', ['lint', 'simplemocha']);
+
+    // override the default test target
+    grunt.registerTask('test', 'simplemocha');
 
 };
