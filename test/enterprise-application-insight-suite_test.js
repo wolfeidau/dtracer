53e842e499fd8fff09906d820c9eefae5c3a1d5d
Mark Wolfe <mark@wolfe.id.au>
2013-01-06 17:07:15 +1100

    Now works.

--- test/enterprise-application-insight-suite_test.js
+++ test/enterprise-application-insight-suite_test.js
@@ -1,26 +1,36 @@
 var tracer = require('../lib/enterprise-application-insight-suite.js')
     , should = require('should')
 
-describe('tracer', function(){
+describe('tracer', function () {
 
-    describe('version', function(){
-        it('should exist', function(){
-                should.exist(tracer.version)
+    describe('version', function () {
+
+        it('should exist', function () {
+            should.exist(tracer.version)
+        })
+    })
+
+    describe('add provider', function () {
+
+        it('should exist', function () {
+            should.exist(tracer.dtraceProvider)
+        })
+
+    })
+
+    describe('add probe', function () {
+
+        it('should succeed', function () {
+            tracer.addProbe("startChild", "int", "char*")
+            tracer.enable()
+        })
+
+        it('should have registered a method to fire the probe', function () {
+            tracer.probes.startChild(function () {
+                return 1
             })
         })
 
-    describe('add provider', function(){
-        it('should exist', function(){
-            should.exist(tracer.dtraceProvider)
-        })
     })
-
-    describe('add probe', function(){
-        it('should succeed', function(){
-            tracer.addProbe("startChild", "int", "char*")
-            tracer.dtraceProvider.enable()
-        })
-    })
-
 
 })
